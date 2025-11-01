cc998c57b44e4146d430390d6823b7211cda2821
mohammedkhubaib-github10 <138954346+mohammedkhubaib-github10@users.noreply.github.com>
2025-11-01 17:44:36 +0530

    Change database URL from MySQL to PostgreSQL

--- core/database.py
+++ core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_Url = "mysql+pymysql://root:22MIS0125@localhost/backend"
+DB_Url = "postgresql://yourkart_user:5rS66n1rzzqbZDgZhggO3R9dnae3faqW@dpg-d42vh8odl3ps73cr5uig-a/yourkart"
 
 engine = create_engine(DB_Url)
 
