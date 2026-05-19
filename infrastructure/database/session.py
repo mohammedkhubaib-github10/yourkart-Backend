b10357b4cf221e55e27df33fb2857c0a664b0547
Mohammed Khubaib C <mohammedkhubaib10@gmail.com>
2026-05-19 21:03:07 +0530

    Containerization is done

--- infrastructure/database/session.py
+++ infrastructure/database/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_Url = "mysql+pymysql://root:22MIS0125@localhost/backend"
+DB_Url = "mysql+pymysql://root:root@mysql-container:3306/backend"
 engine = create_engine(DB_Url)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
